1a78185901f282861def5cfb1fe14ef8253fa96c
sushantpatil03 <sushantofficial03@gmail.com>
2023-01-19 22:36:06 +0530

    changes

--- script.js
+++ script.js
@@ -3,17 +3,29 @@ function validate() {
     let email = document.getElementById("email").value;
     let phone = document.getElementById("phone").value;
 
+    let invalid = false
     let text = "Please enter ";
     if (name == null || name == "") {
-        text = text+"name,";
+        text = text+"Name,";
+        invalid = true;
     }
     if (email == null || email == "") {
-        text = text+"email,";
+        text = text+"Email,";
+        invalid = true;
     }
     if (phone == null || phone == "") {
-        text = text+"phone,";
+        text = text+"Phone,";
+        invalid = true;
+    }else if(phone.length <10){
+        alert("Your contact number must contain 10 digits!");
+        return false;
     }
 
-    alert(text);
+    if(invalid){
+        alert(text);
+    }else{
+        alert("Thanks for joining! Our team will call back you very soon!");
+        return true;
+    }
     return false
 }
